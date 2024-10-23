e4406dfe7b997008c2340ab85773b79f54b04324
Phuong Anh Mai Tran <phanhforwork25@gmail.com>
2024-10-24 00:19:33 +0700

    Complete assignment 2.2.5

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        String strNum1 = scanner.nextLine();
+        
+        System.out.print("Enter the second number: ");
+        String strNum2 = scanner.nextLine();
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            System.out.println("Quotient: " + quotient);
+        } else {
+            System.out.println("Quotient: Division by zero is undefined.");
+        }
+    }
+}
