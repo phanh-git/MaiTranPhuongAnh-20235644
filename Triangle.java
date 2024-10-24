08340d196e72e9377e32aa17d05a323243146e26
Phuong Anh Mai Tran <phanhforwork25@gmail.com>
2024-10-24 09:32:32 +0700

    Complete exercise 3

---
+++ Triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the height of the triangle (n): ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if (j >= n - i + 1 && j <= n + i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
